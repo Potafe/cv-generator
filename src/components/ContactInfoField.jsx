ba16a9d0b600680b006eae0df83b2fa0f262e971
Potafe <bt21me011@nitmz.ac.in>
2024-02-11 16:56:22 +0530

    fix: fixed contact info not getting edited

--- src/components/ContactInfoField.jsx
+++ src/components/ContactInfoField.jsx
@@ -11,19 +11,19 @@ function ContactInfoField({ contactinfo, handleChange }) {
                     <span>
                         Email Address: 
                     </span>
-                    <input type = "text" placeholder="Email" name = "Email" className="input-field" value = {contactinfo.email} onChange={handleChange} data-custom-value="contactinfo"/>
+                    <input type = "text" placeholder="Email" name = "email" className="input-field" value = {contactinfo.email} onChange={handleChange} data-custom-value="contactinfo"/>
                     <span>
                         LinkedIn: 
                     </span>
-                    <input type = "text" placeholder="LinkedIn" name = "LinkedIn" className="input-field" value = {contactinfo.linkedin} onChange={handleChange} data-custom-value="contactinfo"/>
+                    <input type = "text" placeholder="LinkedIn" name = "linkedin" className="input-field" value = {contactinfo.linkedin} onChange={handleChange} data-custom-value="contactinfo"/>
                     <span>
                         Website: 
                     </span>
-                    <input type = "text" placeholder="Website" name = "Website" className="input-field" value = {contactinfo.website} onChange={handleChange} data-custom-value="contactinfo"/>
+                    <input type = "text" placeholder="Website" name = "website" className="input-field" value = {contactinfo.website} onChange={handleChange} data-custom-value="contactinfo"/>
                     <span>
                         Github: 
                     </span>
-                    <input type = "text" placeholder="Github" name = "Github" className="input-field" value = {contactinfo.github} onChange={handleChange} data-custom-value="contactinfo"/>
+                    <input type = "text" placeholder="Github" name = "github" className="input-field" value = {contactinfo.github} onChange={handleChange} data-custom-value="contactinfo"/>
                 </div>
             </fieldset>
         </form>
