bd59c1486698318b3d6e207dfa7481b5ca7d4dc3
Potafe <bt21me011@nitmz.ac.in>
2024-03-11 17:10:29 +0530

    refactor: add appropriate input styling class name

--- src/components/ContactInfoField.jsx
+++ src/components/ContactInfoField.jsx
@@ -1,29 +1,38 @@
+import styles from '../styles/style.module.css'
 function ContactInfoField({ contactinfo, handleChange }) {
     return (
         <form>
             <h3>Contact Information</h3>
-            <fieldset>
-                <div>
-                    <span>
+            <fieldset className={styles.fieldSet}>
+                <div className={styles.inputFieldContainer}>
+                    <span className={styles.inputLabel}>
                         Phone: 
-                    </span>
-                    <input type = "text" placeholder="Phone" name = "phone" className="input-field" value = {contactinfo.phone} onChange={handleChange} data-custom-value="contactinfo"/>
-                    <span>
+                    </span>   
+                    <input type = "text" placeholder="Phone" name = "phone" className={styles.inputField} value = {contactinfo.phone} onChange={handleChange} data-custom-value="contactinfo"/>
+                </div>
+                <div className={styles.inputFieldContainer}>
+                    <span className={styles.inputLabel}>
                         Email Address: 
                     </span>
-                    <input type = "text" placeholder="Email" name = "email" className="input-field" value = {contactinfo.email} onChange={handleChange} data-custom-value="contactinfo"/>
-                    <span>
+                    <input type = "email" placeholder="Email" name = "email" className={styles.inputField}  value = {contactinfo.email} onChange={handleChange} data-custom-value="contactinfo"/>
+                </div>
+                <div className={styles.inputFieldContainer}>
+                    <span className={styles.inputLabel}>
                         LinkedIn: 
                     </span>
-                    <input type = "text" placeholder="LinkedIn" name = "linkedin" className="input-field" value = {contactinfo.linkedin} onChange={handleChange} data-custom-value="contactinfo"/>
-                    <span>
+                    <input type = "text" placeholder="LinkedIn" name = "linkedin" className={styles.inputField}  value = {contactinfo.linkedin} onChange={handleChange} data-custom-value="contactinfo"/>
+                </div>
+                <div className={styles.inputFieldContainer}>
+                    <span className={styles.inputLabel}>
                         Website: 
                     </span>
-                    <input type = "text" placeholder="Website" name = "website" className="input-field" value = {contactinfo.website} onChange={handleChange} data-custom-value="contactinfo"/>
-                    <span>
+                    <input type = "text" placeholder="Website" name = "website" className={styles.inputField}  value = {contactinfo.website} onChange={handleChange} data-custom-value="contactinfo"/>
+                </div>
+                <div className={styles.inputFieldContainer}>
+                    <span className={styles.inputLabel}>
                         Github: 
                     </span>
-                    <input type = "text" placeholder="Github" name = "github" className="input-field" value = {contactinfo.github} onChange={handleChange} data-custom-value="contactinfo"/>
+                    <input type = "text" placeholder="Github" name = "github" className={styles.inputField}  value = {contactinfo.github} onChange={handleChange} data-custom-value="contactinfo"/>
                 </div>
             </fieldset>
         </form>
