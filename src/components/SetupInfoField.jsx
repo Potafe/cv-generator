eaec53160806afbc2b062d4258fc75be1ba1012d
Potafe <bt21me011@nitmz.ac.in>
2024-03-11 17:09:37 +0530

    refactor: removed ugly code

--- src/components/SetupInfoField.jsx
+++ src/components/SetupInfoField.jsx
@@ -1,36 +1,42 @@
 import styles from '../styles/style.module.css'
 
 function SetupInfoField({ setupinfo, handleChange}) {
-    return (
+    const fieldContent = (
         <form>
-            <h3>Basic Information</h3>
-            <fieldset>
-                <div>
-                    <span>
+            <fieldset className={styles.fieldset}>
+                <div className={styles.infoFieldContainer}>
+                    <span className={styles.inputLabel}>
                         First Name: 
                     </span>
                     <input type = 'text' placeholder="First Name" name = 'firstName' value = {setupinfo.firstName} onChange={handleChange} data-custom-value = 'setupinfo' className={styles.inputField} />
                 </div>
-                <div>
-                    <span> 
+                <div className={styles.infoFieldContainer}>
+                    <span className={styles.inputLabel}> 
                         Last Name: 
                     </span>
                     <input type = 'text' placeholder="Last Name" name = 'lastName' value = {setupinfo.lastName} onChange={handleChange} data-custom-value = 'setupinfo' className={styles.inputField}/>
                 </div>
-                <div>
-                    <span> 
+                <div className={styles.infoFieldContainer}>
+                    <span className={styles.inputLabel}> 
                         Professional Title: 
                     </span>
                     <input type="text" placeholder="Professional Title" name="profTitle" value={setupinfo.profTitle} onChange={handleChange} data-custom-value = 'setupinfo' className={styles.inputField}/>
                 </div>
-                <div>
-                    <span>
+                <div className={styles.infoFieldContainer}>
+                    <span className={styles.inputLabel}>
                         About You: 
                     </span>
                     <input type="text" placeholder="About You" name="aboutYou" value={setupinfo.aboutYou} onChange={handleChange} data-custom-value = 'setupinfo' className={styles.inputField}/>
                 </div>
             </fieldset>
         </form>
+    )
+
+    return (
+       <div>
+        <h3>Basic Information</h3>
+        {fieldContent}
+       </div> 
     );
 }
 
